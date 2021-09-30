30fa50196c4813c64d42ec9103c334ed4011669e
Zas Mama <104ambrown@gmail.com>
2021-09-30 19:44:06 -0400

    Started the barebones structure/ #notes in the main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,33 @@
-Py Bank Main.py
+# First we need to import the csv file of our data
+
+# Next we need to establish the path
+
+# I think we need to define variables here? Maybe? Or maybe we creates lists to store the data, is there a difference?
+
+# Count the things, and by things, I mean the number of months in the data set
+    # find the first row of data
+    # find the last row of data
+
+# Calculate the net gain or loss over the period
+    # Find the first value in the gain or loss column
+    # Find the last value in the gain or loss column 
+    # calculate using those values the net gain or loss
+
+#Calculate the average of changes in "Profit/Losses" over the time period
+    # Now how the fuck do we do that arghhh
+    # Do some fancy-schmancy calculations I guess... computer do the thing!!!
+
+# Greatest increase in profits,
+    # Use a while loop to look for greatest value
+    # Print greatest value
+    # Print date it occured
+    # Print amount
+
+# Greatest decrease in losses
+    # Use a while loop to look for lowest value
+    # Print lowest value
+    # Print date it occured
+    # Print amount
+
+# We need to print the analysis to terminal
+# Lastly we need to export our analysis as a text file 
