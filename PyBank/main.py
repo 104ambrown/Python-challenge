34bc0e80068f8564860faa8686d630702931ec22
Zas Mama <104ambrown@gmail.com>
2021-10-02 15:18:32 -0400

    added code to find and print the greatest increase and greatest decrease, then to print the results with the date that they occured. Created a couple dictionaries... I think.

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,13 +7,14 @@ import csv
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
 # I think we need to define variables here? Maybe? Or maybe we creates lists to store the data, is there a difference?
-Date = [1]
-"Profit/Losses" = [2]
+Date = {0}
+"Profit/Losses" = {1}
+#I think I just created 2 dictionaries and told them what col
 
-# open the file
+# open the file, read it, write it, do something to it *le shrug*
 with open(budget_data.csv) as csvfile:
     csvreader - csv.reader(csvfile, delimiter=",")
-    for row it csvreader
+    for row in csvreader
         # Add Date
         Date.append(row[0])
         # Add Profit/Losses
@@ -26,17 +27,19 @@ with open(budget_data, "w") as datafile:
 
 
 # Count the things, and by things, I mean the number of months in the data set
-   def months(months):
+   def months(Date):
        print(f"The number of months in the data set are...")
        for value in months
             print(value)
-        print("There are " + len(months))
+        print("After ", + len(Date) + " long months at sea I finally made landfall on the southern shores of the dataset")
 
 
 # Calculate the net gain or loss over the period
     # Find the first value in the gain or loss column
     # Find the last value in the gain or loss column 
     # calculate using those values the net gain or loss
+        # or I guess we could just use a simple sum command, screw you VBA you high maintenance asshole
+
 
 #Calculate the average of changes in "Profit/Losses" over the time period
     
@@ -46,12 +49,18 @@ with open(budget_data, "w") as datafile:
     # Print greatest value
     # Print date it occured
     # Print amount
+    maximum = max{"Profit/Losses"}
+        print(maximum)
+        print("I won big betting on ponies and made ", + maximum, + " dollars on ", + Date)
 
 # Greatest decrease in losses
     # Use a while loop to look for lowest value
     # Print lowest value
     # Print date it occured
     # Print amount
+    minimum = min{"Profit/Losses"}
+        print(minimum)
+        print("I hit rock bottom betting on ponies and lost ", + minimum, + "dollars on ", + Date)
 
 # We need to print the analysis to terminal
 # Lastly we need to export our analysis as a text file 
